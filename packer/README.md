7b56aa9304adb2d8423aec96fb0232d550ba61e6
Windemiatrix <r.martsev@gmail.com>
2021-05-05 20:00:16 +0300

    Creating Debian 10 template on VMWare with Packer

---
+++ packer/README.md
@@ -0,0 +1,9 @@
+# Packer templates for vmware
+
+Thanks to [guillermo-musumeci](https://github.com/guillermo-musumeci/packer-vsphere-iso-linux/tree/master/Debian-10).
+
+To create template execute:
+
+``` bash
+packer build -var-file variables.json ubuntu1804.json
+```
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "vsphere_user" {
+  default     = ""
+  description = "VSphere user"
+}
+
+variable "vsphere_password" {
+  default     = ""
+  description = "VSphere password"
+}
+
+variable "vsphere_server" {
+  default     = ""
+  description = "VSphere server"
+}
